c8fdb3d4c4d7d97dd54e0bf30a18cd4e2649e932
merahn <merahn@iu.edu>
2023-02-22 16:05:23 -0500

    finshed stuff

--- app.py
+++ app.py
@@ -3,17 +3,30 @@ import csv
 
 app = Flask(__name__)
 
-#globals 
-greetings = ['How are you?', 'What is the weather like out there?', 'Is it Friday yet?']
-
 def get_dinos():
     with open('dinosaurs.csv', 'r') as csvfile:
         data=csv.DictReader(csvfile)
         dinosaurs={row['slug']: {'name': row['name'], 'description':row['description'], 'image':row['image'], 'image-credit':row['image-credit'], 'source-url':row['source-url'], 'source-credit':row['source-credit']} for row in data}
     return dinosaurs
 
+# @app.route('/')
+# def index():
+#     dinosaurs=get_dinos()
+#     print(dinosaurs)
+#     return render_template('index.html', dinosaurs=dinosaurs)
+
 @app.route('/')
-def index():
+@app.route('/dino')
+@app.route('/dino/<dino>')
+def index(dino=None):
     dinosaurs=get_dinos()
-    print(dinosaurs)
-    return render_template('index.html', dinosaurs=dinosaurs)
+    if dino in dinosaurs.keys():
+        print(dino)
+        return render_template('dino.html', dinosaur=dinosaurs[dino])
+    else:
+        return render_template('index.html', dinosaurs=dinosaurs)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
