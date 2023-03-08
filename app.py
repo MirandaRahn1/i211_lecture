5a2dc0d285b79e8b7db9217cc07fb7f8ae2613ae
merahn <merahn@iu.edu>
2023-03-08 13:51:33 -0500

    dino

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 import csv
 
 app = Flask(__name__)
 
-
 DINO_PATH = app.root_path + '/dinosaurs.csv'
 FAQ_PTH = app.root_path + '/faq.csv'
+DINO_PATH = app.root_path + '/dinosaurs.csv'
+DINO_KEYS = ['slug', 'name', 'description', 'image', 'image-credit', 'source-url', 'source-credit']
 
 def get_dinos():
     with open(DINO_PATH, 'r') as csvfile:
         data=csv.DictReader(csvfile)
         dinosaurs={row['slug']: {'name': row['name'], 'description':row['description'], 'image':row['image'], 'image-credit':row['image-credit'], 'source-url':row['source-url'], 'source-credit':row['source-credit']} for row in data}
     return dinosaurs
+
+def set_dinos(dinosaurs):
+    try:
+        with open(DINO_PATH, mode='w', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=DINO_KEYS)
+            writer.writeheader()
+            for dino in dinosaurs.values():
+                writer.writerow(dino)
+    except Exception as err:
+        print(err)
 
 def get_faq():
     fileList = []
@@ -27,7 +38,6 @@ def get_faq():
 #     print(dinosaurs)
 #     return render_template('index.html', dinosaurs=dinosaurs)
 
-# print(get_faq())
 @app.route('/')
 @app.route('/dino')
 @app.route('/dino/<dino>')
@@ -48,3 +58,20 @@ def about():
 def faq():
     faq = get_faq()
     return render_template('faq.html', faq = faq)
+
+@app.route('/add-dino', methods=['GET', 'POST'])
+def add_dino():
+    if request.method == 'POST':
+        dinosaurs = get_dinos()
+        new_dino = {}
+        new_dino['name'] = request.form['name']
+        new_dino['description'] = request.form['description']
+        new_dino['image'] = request.form['image']
+        new_dino['image-credit'] = request.form['image-credit']
+        new_dino['source-url'] = request.form['source-url']
+        new_dino['source-credit'] = request.form['source-credit']
+        dinosaurs[request.form['slug']] = new_dino
+        set_dinos(dinosaurs)
+        return redirect(url_for('index'))
+    else:
+        return render_template('add_dino.html')
